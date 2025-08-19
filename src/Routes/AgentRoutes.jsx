21b6ded45908e70ef9acd6bb659b4a1f9d7079f3
ibrahimimran777 <ibrahimimraniu@gmail.com>
2025-08-19 15:31:21 +0500

    Agent Dashboard Working Except Feedback Box

--- src/Routes/AgentRoutes.jsx
+++ src/Routes/AgentRoutes.jsx
@@ -1,14 +1,17 @@
 // src/routes/AgentRoutes.jsx
-import { Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import AgentLayout from "../pages/AgentPortal/AgentLayout";
 import AgentDashboard from "../components/Agent/AgentDashboard";
 import AgentFeedbackBox from "../components/Agent/AgentFeedbackBox";
 
 const AgentRoutes = ({ setIsLoggedIn }) => (
-  <Route element={<AgentLayout setIsLoggedIn={setIsLoggedIn} />}>
-    <Route index element={<AgentDashboard />} />
-    <Route path="feedback" element={<AgentFeedbackBox />} />
-  </Route>
+  <Routes>
+    <Route element={<AgentLayout setIsLoggedIn={setIsLoggedIn} />}>
+      <Route index element={<AgentDashboard />} />
+      <Route path="feedback" element={<AgentFeedbackBox />} />
+    </Route>
+  </Routes>
 );
 
 export default AgentRoutes;
+
