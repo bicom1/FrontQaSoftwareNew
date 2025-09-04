d13571e268b1bcbd0a244a94adf5fecbc8b50a6f
haris <qc_dev@bicommunications.net>
2025-09-04 18:19:41 +0500

    Fix case sensitivity for Overview

--- src/features/config.js
+++ src/features/config.js
@@ -1,4 +1,6 @@
 export const baseUrl =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:3001";
+  // import.meta.env.VITE_API_BASE_URL || "http://localhost:3001";
+  import.meta.env.VITE_API_BASE_URL || "https://backendqasoftware-k62i.onrender.com";
+
 
 export const getToken = () => localStorage.getItem("token");
